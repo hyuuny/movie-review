5a761cb54663cfa2b78495672d23eb17e5b5fcb7
setge <shyune@knou.ac.kr>
2021-02-11 00:54:17 +0900

    썸네일&파일 삭제 기능 커밋

--- src/main/java/com/setge/talkingtoday/controller/ImageFileUploadController.java
+++ src/main/java/com/setge/talkingtoday/controller/ImageFileUploadController.java
@@ -2,6 +2,7 @@ package com.setge.talkingtoday.controller;
 
 import com.setge.talkingtoday.dto.UploadResultDTO;
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -14,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -44,8 +46,6 @@ public class ImageFileUploadController {
             }
 
             String fileName = uploadFile.getOriginalFilename();
-//            String fileName = originalName.substring(originalName.lastIndexOf("/" + 1));
-//            String fileName = originalName;
             log.info("fileName : " + fileName);
 
             String folderPath = makeFolder();
@@ -54,8 +54,21 @@ public class ImageFileUploadController {
             Path savePath = Paths.get(saveName);
 
             try {
-                uploadFile.transferTo(savePath); // 실제 이미지 저장
+                //원본 파일 저장
+                uploadFile.transferTo(savePath);
+
+                //섬네일 생성
+                String thumbnailSaveName = uploadPath + File.separator + folderPath + File.separator
+                        + "s_" + uuid + "_" + fileName;
+                log.info("thumbnailFileName : " + thumbnailSaveName);
+
+                //섬네일 파일 이름은 중간에 s_로 시작하도록
+                File thumbnailFile = new File(thumbnailSaveName);
+
+                //섬네일 생성
+                Thumbnailator.createThumbnail(savePath.toFile(), thumbnailFile, 100, 100);
                 resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -78,7 +91,6 @@ public class ImageFileUploadController {
     }
 
     /**
-     *
      * @param fileName(Encoding)
      * @return byte[]
      */
@@ -101,5 +113,26 @@ public class ImageFileUploadController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return result;
+    }
+
+    @PostMapping("/removeFile")
+    public ResponseEntity<Boolean> removeFile(String fileName) {
+
+        String srcFileName = "";
+
+        try {
+            srcFileName = URLDecoder.decode(fileName, "UTF-8"); // aJax로 넘어온 파일 이름을 디코딩하여 저장
+            File file = new File(uploadPath + File.separator + srcFileName);
+            boolean result = file.delete(); // 삭제에 성공하면 true
+            File thumbnail = new File(file.getParent(), "s_" + file.getName()); // 같은 이름의 썸네일을 저장
+            result = thumbnail.delete(); // 썸네일을 삭제하면 true
+
+            return new ResponseEntity<>(result, HttpStatus.OK);
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 }
